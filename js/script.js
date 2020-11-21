3a302ef50a17a77dd1877fdb7f013a2d6db82c93
Robert Thompson <bob@jazzviolin.com>
2020-11-20 21:10:28 -0800

    Formatted JS

--- js/script.js
+++ js/script.js
@@ -14,17 +14,15 @@ let clear = () => {
   operator = undefined;
   memory = undefined;
   total = undefined;
-} 
-// #TODO - Fix adding multiple leading Zeros issue
+}
 
 function displayResults(result) {
   if (result.toString().length > 10) {
-     calcDisplay.innerText = result.toString().substring(0, 11)
+    calcDisplay.innerText = result.toString().substring(0, 11)
   } else {
-  calcDisplay.innerText = result.toString()
+    calcDisplay.innerText = result.toString()
+  }
 }
-}
-
 
 let operate = (operator, a, b) => {
   switch (operator) {
@@ -43,14 +41,13 @@ let operate = (operator, a, b) => {
     case 'divide':
       if (a == 0) {
         clear()
-        calcDisplay.innerText = "u no div by 0";        
+        calcDisplay.innerText = "u no div by 0";
         break;
       }
       total = divide(a, b);
       displayResults(total)
       break;
   }
-  // clean up memory
   active = undefined;
   memory = undefined;
   operator = undefined;
@@ -58,10 +55,8 @@ let operate = (operator, a, b) => {
 }
 
 
-// Targets
 let calcDisplay = document.querySelector('.display p')
 
-// Functions
 let updateActive = (num) => {
   if (num == "%") {
     active /= 100
@@ -70,88 +65,85 @@ let updateActive = (num) => {
     total = 0
   } else if (active == undefined) {
     active = num;
-   } else {
+  } else {
     active = active.concat(num);
-   }
+  }
   calcDisplay.innerText = active
   return active
- }
+}
 
 let clearAll = document.querySelector('.clear')
   .addEventListener('click', clear)
 
 let btnZero = document.querySelector('.zero')
-  .addEventListener('click', () => 
-   screen.innerText = updateActive("0"))
+  .addEventListener('click', () =>
+    screen.innerText = updateActive("0"))
 
 let btnOne = document.querySelector('.one')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("1"))
+    screen.innerText = updateActive("1"))
 
 let btnTwo = document.querySelector('.two')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("2"))
+    screen.innerText = updateActive("2"))
 
 let btnThree = document.querySelector('.three')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("3"))
+    screen.innerText = updateActive("3"))
 
 let btnFour = document.querySelector('.four')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("4"))
+    screen.innerText = updateActive("4"))
 
 let btnFive = document.querySelector('.five')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("5"))
+    screen.innerText = updateActive("5"))
 
 let btnSix = document.querySelector('.six')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("6"))
+    screen.innerText = updateActive("6"))
 
 let btnSeven = document.querySelector('.seven')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("7"))
+    screen.innerText = updateActive("7"))
 
 let btnEight = document.querySelector('.eight')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("8"))
+    screen.innerText = updateActive("8"))
 
 let btnNine = document.querySelector('.nine')
   .addEventListener('click', () =>
-  screen.innerText = updateActive("9"))
+    screen.innerText = updateActive("9"))
 
 let btnDecimal = document.querySelector('.decimal')
   .addEventListener('click', () => {
     if (!calcDisplay.innerText.includes('.')) {
-    screen.innerText = updateActive(".")
+      screen.innerText = updateActive(".")
     }
   })
 
-
-// This processes the memory fields. Based on which order operators or numbers
-// are entered, it decides what to do with them
 let processOperator = (typeOfOperator) => {
-   if (!active && !memory && total) {
+  if (!active && !memory && total) {
     memory = total;
     operator = typeOfOperator
-  // Haven't pressed Equals yet
-  } else if (active && memory && operator)  {
+    // Haven't pressed "Equals" yet
+  } else if (active && memory && operator) {
     total = operate(typeOfOperator, active, memory)
     active = undefined
     operator = typeOfOperator
     memory = total
-  // String of operatores in a row
+    // String of operatores in a row
   } else if (!active && !memory && !operator) {
     total += operate(typeOfOperator, active, memory)
     operator = typeOfOperator
     memory = total
     active = undefined
-    // First entry
   } else if (!active && !memory && total === 0) {
     memory = total
     operator = typeOfOperator
     active = undefined
-  } else if (!memory)  {
+    // First entry
+  } else if (!memory) {
     memory = active;
     operator = typeOfOperator;
     active = undefined
@@ -165,20 +157,19 @@ let blink = () => {
   }, 75);
 }
 
-// OPERATORS
 let operatorDivide = document.querySelector('.divide')
   .addEventListener('click', function () {
     blink();
     total = processOperator(operator)
     operator = 'divide'
-});
+  });
 
 let operatorMultiply = document.querySelector('.multiply')
   .addEventListener('click', function () {
     blink();
     total = processOperator(operator)
-    operator = 'multiply'  
-});
+    operator = 'multiply'
+  });
 
 let operatorSubtract = document.querySelector('.subtract')
   .addEventListener('click', function () {
@@ -188,10 +179,10 @@ let operatorSubtract = document.querySelector('.subtract')
     }
     total = processOperator(operator)
     operator = 'subtract'
-});
+  });
 
-let operatorAdd = document.querySelector('.add') 
-  .addEventListener('click', function() {
+let operatorAdd = document.querySelector('.add')
+  .addEventListener('click', function () {
     blink();
     if (!operator) {
       operator = 'add'
@@ -202,7 +193,7 @@ let operatorAdd = document.querySelector('.add')
     }
     total = processOperator(operator)
     operator = 'add'
-});
+  });
 
 let operatorPercentage = document.querySelector('.percentage')
   .addEventListener('click', function () {
@@ -217,24 +208,24 @@ let operatorPercentage = document.querySelector('.percentage')
 
   });
 
-  let operatorPlusMinus = document.querySelector('.plusminus')
-    .addEventListener('click', function () {
-      blink();
-      if (active) {
-        active *= -1
-        calcDisplay.innerText = active
-      } else if (!active && total) {
-        total *= -1
-        displayResults(total)
-      }
+let operatorPlusMinus = document.querySelector('.plusminus')
+  .addEventListener('click', function () {
+    blink();
+    if (active) {
+      active *= -1
+      calcDisplay.innerText = active
+    } else if (!active && total) {
+      total *= -1
+      displayResults(total)
+    }
   })
 
 let operatorEquals = document.querySelector('.equals')
-  .addEventListener('click', function() {
+  .addEventListener('click', function () {
     blink();
     if (total == undefined) {
       total = operate(operator, parseFloat(active), parseFloat(memory))
     } else if (total == 0) {
       total += operate(operator, parseFloat(active), parseFloat(memory))
-    } 
-});
+    }
+  })
